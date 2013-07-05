ae0fad83689ae1785a2869b6ef5d443c4865210e
Lucas Falk Beier <lucas.beier@gmail.com>
2013-07-05 01:43:01 -0300

    Refactoring.

--- src/main/java/Controller/ShowThreadController.java
+++ src/main/java/Controller/ShowThreadController.java
@@ -2,47 +2,39 @@ package Controller;
 
 import java.io.IOException;
 import java.util.List;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.swing.text.StyledEditorKit.BoldAction;
-
+import Framework.ApplicationController;
 import Model.Comment;
 import Model.Thread;
-import Model.User;
 
-public class ShowThreadController extends HttpServlet {
+public class ShowThreadController extends ApplicationController {
 	private static final long serialVersionUID = 1L;
 
-    public ShowThreadController() {
-        super();
+	public ShowThreadController() {
+		super();
 
-    }
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		int idThread = Integer.parseInt(request.getParameter("id"));
 		Thread thread = new Thread().findById(idThread);
 		List<Comment> comments = new Comment().findAllByThread(idThread);
-		HttpSession session = ((HttpServletRequest) request).getSession();
-		int idUSer = (Integer) session.getAttribute("idUser");
+		int idUSer = getIdUserLoggedIn(request);
 		boolean canEditThread = new Thread().canUserModifyThread(idUSer, idThread);
-		boolean isAdmin = new User().isUserAdmin(idUSer);
+		boolean isAdmin = isLoggedUserAdmin(request);
 
 		request.setAttribute("isAdmin", isAdmin);
 		request.setAttribute("canEditThread", canEditThread);
 		request.setAttribute("idUSer", idUSer);
 		request.setAttribute("thread", thread);
 		request.setAttribute("comments", comments);
-		RequestDispatcher rs = request.getRequestDispatcher("showThread.jsp");
-		rs.forward(request, response);
+		
+		render("showThread", request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {}
-
 }
